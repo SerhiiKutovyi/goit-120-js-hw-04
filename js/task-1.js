9ad8700d01d10079c30e92a83009a6e1322a8a64
SerhiiKutovyi <wagenrex@gmail.com>
2025-07-25 15:11:21 +0300

    HW-1

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+const logins = ['m4ngoDoge', 'kiwiBad3st', 'PaliIScute', 'WagenRex'];
+const password = ['WagenRex'];
+
+// logins.join(' ').includes(password)
+//   ? console.log(`User ${password} OK!`)
+//   : console.log(`User ${password} error!`);
+
+// for (let i = 0; i < logins.length; i++) {
+//   console.log(logins.includes(password));
