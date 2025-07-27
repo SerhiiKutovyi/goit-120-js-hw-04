c25d6af2cccdcf0bef41dd91f823a5e93523a45d
SerhiiKutovyi <wagenrex@gmail.com>
2025-07-27 14:37:56 +0300

    hw-1

--- js/task-1.js
+++ js/task-1.js
@@ -66,6 +66,21 @@
 //   },
 // };
 
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Henry',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+
+// console.log(apartment);
+
 const apartment = {
   imgUrl: 'https://via.placeholder.com/640x480',
   descr: 'Spacious apartment in the city center',
@@ -79,4 +94,11 @@ const apartment = {
   },
 };
 
-console.log(apartment);
+const ownerName = apartment.owner.name;
+const ownerPhone = apartment.owner.phone;
+const ownerEmail = apartment.owner.email;
+const numberOfTags = apartment.tags.length;
+const firstTag = apartment.tags[0];
+const lastTag = apartment.tags[apartment.tags.length - 1];
+
+console.log(lastTag);
