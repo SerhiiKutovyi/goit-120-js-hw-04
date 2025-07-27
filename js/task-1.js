3a01384d8e68c24d077e14519af69d9ed9d4de3b
SerhiiKutovyi <wagenrex@gmail.com>
2025-07-27 14:07:34 +0300

    hw-1

--- js/task-1.js
+++ js/task-1.js
@@ -23,13 +23,60 @@
 
 // console.log(message);
 
-const logins = [51, 18, 15, 14, 78, 62, 91, 65, 8];
-let num = logins[0];
+// const logins = [51, 18, 15, 14, 78, 62, 91, 65, 8];
+// let num = logins[0];
 
-for (let i = 0; i < logins.length; i++) {
-  if (num < logins[i]) {
-    num = logins[i];
-  }
-}
+// for (let i = 0; i < logins.length; i++) {
+//   if (num < logins[i]) {
+//     num = logins[i];
+//   }
+// }
 
-console.log(num);
+// console.log(num);
+
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   public: true,
+//   rating: 8.38,
+// };
+
+// console.log(typeof genres);
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+// };
+
+// const user = {
+//   name: 'Jacques Gluke',
+//   tag: 'jgluke',
+//   location: {
+//     country: 'Jamaica',
+//     city: 'Ocho Rios',
+//   },
+//   stats: {
+//     followers: 5603,
+//     views: 4827,
+//     likes: 1308,
+//   },
+// };
+
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+  owner: {
+    name: 'Henry',
+    phone: '982-126-1588',
+    email: 'henry.carter@aptmail.com',
+  },
+};
+
+console.log(apartment);
